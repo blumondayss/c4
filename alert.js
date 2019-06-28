e586cebee697791149f73e9b76448195a20205b2
blumondayss <52316117+blumondayss@users.noreply.github.com>
2019-06-28 05:29:08 -0400

    create

---
+++ alert.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name           C4 - NEWWWWWWWW
+// @version 4.7
+// @namespace      cam4_goes_droopy
+// @description    cam4 cleanup
+// @include       http://*.cam4.com/*
+// @include       https://*.cam4.com/*
+// @include       https://www.cam4.com/*
+// @require       http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
+// @require       https://raw.githubusercontent.com/kylelder/kylelder.github.io/master/mousetrap.min.js
+// @require       https://www.jqueryscript.net/demo/Easy-jQuery-Progress-Bar-Timer-Plugin-For-Bootstrap-3-progressTimer/js/jquery.progressTimer.js
+// @grant       GM_xmlhttpRequest
+// @run-at         document-start
+// ==/UserScript==
+
+
+
+
+$(function(){
+
+    alert('userScript loaded');
+
+});
+
+
+
+
+
+
+//.user.js
